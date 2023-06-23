b9cf429b632fd042d1dacf9a599acd62f65dc5e6
higor <higorlinosilva1@gmail.com>
2023-06-23 10:54:40 -0300

    finalizado

---
+++ cmake-build-debug/CMakeFiles/TrabalhoED.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrabalhoED.dir/FilaNavios.c.o"
+  "CMakeFiles/TrabalhoED.dir/FilaNavios.c.o.d"
+  "CMakeFiles/TrabalhoED.dir/funcoesPorto.c.o"
+  "CMakeFiles/TrabalhoED.dir/funcoesPorto.c.o.d"
+  "CMakeFiles/TrabalhoED.dir/main.c.o"
+  "CMakeFiles/TrabalhoED.dir/main.c.o.d"
+  "CMakeFiles/TrabalhoED.dir/pilhaContainers.c.o"
+  "CMakeFiles/TrabalhoED.dir/pilhaContainers.c.o.d"
+  "TrabalhoED"
+  "TrabalhoED.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TrabalhoED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TrabalhoED.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TrabalhoED.
+# This may be replaced when dependencies are built.
